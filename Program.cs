a170b28fee0a6b33336dfec6c0117725451d2235
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-09-15 15:06:53 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=dev;Password=@DevTeste123";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            ReadUsers(connection);
+            ReadRoles(connection);
+            ReadTags(connection);
+            ReadCategories(connection);
+            connection.Close();
+        }
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadCategories(SqlConnection connection)
+        {
+            var repository = new Repository<Category>(connection);
+            var items = repository.Get();
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+    }
+}
